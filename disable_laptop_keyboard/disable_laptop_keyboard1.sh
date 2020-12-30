0b86ba1fc5a95883fafeaefbb3890205ec6149a2
Nandulal Krishna <60590327+nlkguy@users.noreply.github.com>
2020-12-30 17:33:43 +0530

    Update and rename disable_laptop_keyboard.sh to disable_laptop_keyboard1.sh

--- disable_laptop_keyboard/disable_laptop_keyboard1.sh
+++ disable_laptop_keyboard/disable_laptop_keyboard1.sh
@@ -10,16 +10,17 @@ echo "           (__)\       )\/\\"
 echo "               ||--WWW |"
 echo "               ||     ||"
 
-
+#---------------------------------------------------------------------
 #txt=$(xinput | grep "AT Translated Set 2 keyboard")
 #txt_trim=$(echo $txt | sed -e 's/^[[:space:]]*//')
 #ID=${txt_trim:34:2}
 #echo "ID of the internal keyboard is ${txt_trim:34:2}"
+#---------------------------------------------------------------------
 echo "do you want to disable it (y or n) : " && read confirmation
 yes="y"
 no="n"
 if [ $confirmation == $yes ]; then
-	xinput disable AT Translated Set 2 keyboard
+	xinput disable "AT Translated Set 2 keyboard"
 	echo " _________________________________________"
 	echo "/ notice any diffrence ? your keyboard is \\"
 	echo "\ disabled, mine toooo, have a nice day   /"
